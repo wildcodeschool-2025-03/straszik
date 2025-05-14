0eec3bb6a36114b4c60ec846346dd79aae58d1e9
Maxime Giampaoli <maxime.giampaoli@gmail.com>
2025-05-14 10:54:50 +0200

    US_19_Admin_page - fonctionnalités ok

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,4 +1,6 @@
 import Header from "../../components/Header/Header";
+import NewsletterList from "../../components/NewsletterList/NewsletterList";
+import UserList from "../../components/UserList/UserList";
 
 function Admin() {
   function handleDisconnect() {
@@ -12,11 +14,13 @@ function Admin() {
       <h1 className="text-secondary text-4xl font-extrabold text-center lg:text-5xl mt-10 mb-14">
         ESPACE ADMINISTRATEUR
       </h1>
-      <section className="flex flex-col items-center gap-1 mt-4">
+      <UserList />
+      <NewsletterList />
+      <section className="flex flex-col items-center gap-1 mt-16">
         <button
+          onClick={handleDisconnect}
           className="bg-red-600 hover:bg-red-700 text-xl rounded-xl p-2 w-52 mb-4 font-semibold hover:scale-110"
           type="submit"
-          onClick={handleDisconnect}
         >
           Se déconnecter
         </button>
